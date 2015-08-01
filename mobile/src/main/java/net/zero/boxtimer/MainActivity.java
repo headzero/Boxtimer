9bc6f7d83034a94011d1affc004a290d1d80b5f6
young(이두영) <young.lee@daumkakao.com>
2015-08-01 17:56:46 +0900

    add button event

--- mobile/src/main/java/net/zero/boxtimer/MainActivity.java
+++ mobile/src/main/java/net/zero/boxtimer/MainActivity.java
@@ -1,16 +1,55 @@
 package net.zero.boxtimer;
 
+import android.app.NotificationManager;
+import android.content.Context;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int MINUTE = 60;
+    private EditText runningMin;
+    private EditText restSec;
+    private Button runButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        runningMin = (EditText) findViewById(R.id.edittext_running_time);
+        restSec = (EditText) findViewById(R.id.edittext_rest_time);
+        runButton = (Button) findViewById(R.id.button_run);
+        runButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if("".equals(runningMin.getText().toString()) || "".equals(restSec.getText().toString())){
+                    return;
+                }
+                int runningTime = Integer.valueOf(runningMin.getText().toString()) * MINUTE;
+                int restTime = Integer.valueOf(restSec.getText().toString());
+                if(runningTime == 0 || restTime == 0){
+                    return;
+                }
+
+                NotificationCompat.Builder mBuilder =
+                        new NotificationCompat.Builder(MainActivity.this)
+                                .setSmallIcon(R.drawable.common_signin_btn_icon_dark)
+                                .setContentTitle("My notification")
+                                .setContentText("Hello World!");
+
+                NotificationManager mNotificationManager =
+                        (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+// mId allows you to update the notification later on.
+                mNotificationManager.notify(0, mBuilder.build());
+            }
+        });
     }
 
     @Override
